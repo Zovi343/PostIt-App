4688db4a9881b89361563c201edf8db919c9d9da
Zovi <39067421+VaelinAlSorna@users.noreply.github.com>
2018-08-23 10:50:31 +0200

    Added fav icon and some comments into server

--- server/server.js
+++ server/server.js
@@ -1,11 +1,15 @@
 const path = require('path');
 const express = require('express');
 const app = express();
+
 const publicPath = path.join(__dirname, '..', 'public');
 const port = process.env.PORT || 3000;
 
+// this servers all assest from public directory
 app.use(express.static(publicPath));
 
+// to make sure that for example /create works I need server index.html for every route
+// it is basically like when we set up historyfallback: true in webpack but we need to do this also with server
 app.get('*', (req, res) => {
   res.sendFile(path.join(publicPath, 'index.html'));
 });
